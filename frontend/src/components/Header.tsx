52bd91d5734fcbb36051bdcd6b3de70dda5a4ad5
Vasya-556 <vkobrin556@gmail.com>
2025-09-30 21:06:13 +0300

    fix(frontend): added name of the user in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import { useIsLoggedIn} from '../isLoggedIn'
+import { getCurrentUserInfo } from "../utils/getCurrentUserInfo";
+
+interface User {
+    id: string;
+    username: string;
+}
 
 function Header() {
     const { isLoggedIn, logout } = useIsLoggedIn();
+    const [thisUserInfo, setThisUserInfo] = useState<User | null>(null);
+
+    useEffect(() => {
+        const fetchThisUser = async () => {
+            const user = await getCurrentUserInfo();
+            if (user) setThisUserInfo(user);
+        };
+    
+        fetchThisUser();
+
+        
+        if (isLoggedIn) {
+            fetchThisUser();
+        }
+    }, [isLoggedIn])
 
   return (
     <header>
         <Link to="/">Home</Link>
       {isLoggedIn? 
       <>
+        <span>Welcome <b>{thisUserInfo?.username}</b></span>
         <button onClick={logout}>logout</button>
       </>: 
       
